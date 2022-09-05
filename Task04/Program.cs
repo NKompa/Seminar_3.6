44d2be1eaaa060d86cd124aa037474c870131114
NKompa <nkompa7@gmail.com>
2022-09-05 22:37:13 +0300

    Fibonacci numbers

---
+++ Task04/Program.cs
@@ -0,0 +1,20 @@
+﻿// // Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int firstNum = 0;
+int secondNum = 1;
+Console.Write(firstNum + " ");
+Console.Write(secondNum + " ");
+
+for (int i = 3; i <=num; i++)
+{
+    int newNum = firstNum + secondNum;
+    Console.Write(newNum + " ");
+    firstNum = secondNum;
+    secondNum = newNum;
+}
